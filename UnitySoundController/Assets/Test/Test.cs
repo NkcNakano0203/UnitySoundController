3916672e47d85ba8a41b932707c1dbdee63a2320
NkcNakano0203 <95272840+NkcNakano0203@users.noreply.github.com>
2024-07-31 23:51:15 +0900

    更新

--- UnitySoundController/Assets/Test/Test.cs
+++ UnitySoundController/Assets/Test/Test.cs
@@ -3,9 +3,13 @@ using Service.SE.Data;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
 using System.Threading;
+using Service.BGM;
 
 public class Test : MonoBehaviour
 {
+    [SerializeField]
+    private MusicPlayer musicPlayer;
+
     [SerializeField]
     private SoundEffect soundEffect;
 
@@ -17,11 +21,15 @@ public class Test : MonoBehaviour
 
     private CancellationTokenSource cts = new();
 
+
     void Start()
     {
+        musicPlayer.Play();
+
         for (int i = 0; i < playCount; i++)
         {
             soundEffectPlayer.PlayAsync(soundEffect, Vector3.zero, cts.Token).Forget();
+
         }
     }
 }
--- UnitySoundController/Assets/UnitySoundController/Scripts/SEPlayer/SoundEffect.cs
+++ UnitySoundController/Assets/UnitySoundController/Scripts/SEPlayer/SoundEffect.cs
@@ -50,19 +50,4 @@ namespace Service.SE.Data
             CurrentPlayCount--;
         }
     }
-
-    public class Disposable : IDisposable
-    {
-        private readonly Action onDispose;
-
-        public Disposable(Action onDispose)
-        {
-            this.onDispose = onDispose;
-        }
-
-        public void Dispose()
-        {
-            onDispose?.Invoke();
-        }
-    }
 }
